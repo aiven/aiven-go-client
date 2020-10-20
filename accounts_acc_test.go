f1ec0619e375c3ef946551fc2a1ba7472e025eed
Ivan Savciuc <ivan.savciuc@gmail.com>
2020-10-20 16:20:54 +0300

    rework accounts acceptance test

--- accounts_acc_test.go
+++ accounts_acc_test.go
@@ -8,23 +8,21 @@ import (
 )
 
 var _ = Describe("Accounts", func() {
-	var (
-		accountName string
-		account     *AccountResponse
-		err         error
-	)
+	Context("Check all posible accounts interactions", func() {
+		// Account
+		var (
+			accountName string
+			account     *AccountResponse
+			err         error
+		)
 
-	BeforeEach(func() {
-		accountName = "test-acc-account" + strconv.Itoa(rand.Int())
-		account, err = client.Accounts.Create(Account{Name: accountName})
-	})
-
-	Context("Create new account", func() {
-		It("should not error", func() {
+		It("Account creation should not error", func() {
+			accountName = "test-acc-account" + strconv.Itoa(rand.Int())
+			account, err = client.Accounts.Create(Account{Name: accountName})
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("should populate fields properly", func() {
+		It("Account creation should populate fields properly", func() {
 			Expect(account.Account.Id).NotTo(BeEmpty())
 			Expect(account.Account.Name).To(Equal(accountName))
 			Expect(account.Account.CreateTime).NotTo(BeNil())
@@ -32,16 +30,15 @@ var _ = Describe("Accounts", func() {
 			Expect(account.APIResponse.Message).To(BeEmpty())
 			Expect(account.APIResponse.Errors).To(BeEmpty())
 		})
-	})
 
-	Describe("AccountTeams", func() {
+		// AccountTeams
 		var (
 			teamName string
 			team     *AccountTeamResponse
 			errT     error
 		)
 
-		BeforeEach(func() {
+		It("Create account team", func() {
 			if account == nil {
 				Fail("account is nil")
 			}
@@ -49,149 +46,99 @@ var _ = Describe("Accounts", func() {
 			team, errT = client.AccountTeams.Create(account.Account.Id, AccountTeam{
 				Name: teamName,
 			})
+
+			Expect(errT).NotTo(HaveOccurred())
+
+			Expect(team.Team.Id).NotTo(BeEmpty())
+			Expect(team.Team.Name).To(Equal(teamName))
+			Expect(team.Team.UpdateTime).NotTo(BeNil())
+			Expect(team.Team.CreateTime).NotTo(BeNil())
+			Expect(team.APIResponse.Message).To(BeEmpty())
+			Expect(team.APIResponse.Errors).To(BeEmpty())
 		})
 
-		Context("Create account team", func() {
-			It("should not error", func() {
-				Expect(errT).NotTo(HaveOccurred())
-			})
+		// Account members
+		var (
+			memberEmail string
+			errM        error
+		)
 
-			It("should populate fields properly", func() {
-				Expect(team.Team.Id).NotTo(BeEmpty())
-				Expect(team.Team.Name).To(Equal(teamName))
-				Expect(team.Team.UpdateTime).NotTo(BeNil())
-				Expect(team.Team.CreateTime).NotTo(BeNil())
-				Expect(team.APIResponse.Message).To(BeEmpty())
-				Expect(team.APIResponse.Errors).To(BeEmpty())
-			})
+		It("should not error", func() {
+			memberEmail = "ivan.savciuc+" + strconv.Itoa(rand.Int()) + "@aiven.io"
+			errM = client.AccountTeamMembers.Invite(account.Account.Id, team.Team.Id, memberEmail)
 
-			var (
-				memberEmail string
-				errM        error
-			)
+			Expect(memberEmail).NotTo(BeEmpty())
+			Expect(errM).NotTo(HaveOccurred())
+		})
 
-			Context("accountTeams", func() {
+		It("should send invite", func() {
+			invites, errI := client.AccountTeamInvites.List(account.Account.Id, team.Team.Id)
+			Expect(errI).NotTo(HaveOccurred())
 
-			})
-			JustBeforeEach(func() {
-				memberEmail = "ivan.savciuc+" + strconv.Itoa(rand.Int()) + "@aiven.io"
-				errM = client.AccountTeamMembers.Invite(account.Account.Id, team.Team.Id, memberEmail)
-			})
-			Context("Invite account team member", func() {
-				It("should not error", func() {
-					Expect(memberEmail).NotTo(BeEmpty())
-					Expect(errM).NotTo(HaveOccurred())
-				})
-
-				It("should be in the team members list", func() {
-
-				})
-
-				It("should send invite", func() {
-					invites, errI := client.AccountTeamInvites.List(account.Account.Id, team.Team.Id)
-					Expect(errI).NotTo(HaveOccurred())
-
-					var found bool
-					for _, invite := range invites.Invites {
-						if invite.UserEmail == memberEmail {
-							found = true
-						}
-					}
-
-					Expect(found).To(Equal(true), "cannot find invitation for newly created member")
-
-					if errD := client.AccountTeamInvites.Delete(account.Account.Id, team.Team.Id, memberEmail); errD != nil {
-						Fail("cannot delete an invitation :" + errD.Error())
-					}
-				})
-			})
-
-			AfterEach(func() {
-				if list, errL := client.AccountTeamMembers.List(account.Account.Id, team.Team.Id); errL != nil {
-					Fail("cannot get a list of account team members:" + errL.Error())
-				} else {
-					for _, m := range list.Members {
-						if m.UserEmail == memberEmail {
-							if errD := client.AccountTeamMembers.Delete(account.Account.Id, team.Team.Id, memberEmail); errD != nil {
-								Fail("cannot delete account team member:" + errD.Error())
-							}
-						}
-					}
+			var found bool
+			for _, invite := range invites.Invites {
+				if invite.UserEmail == memberEmail {
+					found = true
 				}
-			})
+			}
 
-			Context("AccountTeamProjects", func() {
-				var (
-					projectName string
-					projectType string = "admin"
-				)
+			Expect(found).To(Equal(true), "cannot find invitation for newly created member")
 
-				BeforeEach(func() {
-					projectName = "test-acc-pr" + strconv.Itoa(rand.Int())
-
-					By("Create project")
-					if _, errP := client.Projects.Create(CreateProjectRequest{
-						Project:   projectName,
-						AccountId: account.Account.Id,
-					}); errP != nil {
-						Fail("cannot create project :" + errP.Error())
-					}
-
-					By("Create account team project association")
-					if errTP := client.AccountTeamProjects.Create(
-						account.Account.Id,
-						team.Team.Id,
-						AccountTeamProject{ProjectName: projectName, TeamType: projectType}); errTP != nil {
-						Fail("cannot create account team project association:" + errTP.Error())
-					}
-				})
-				Context("Account team project association", func() {
-					It("should be in the list", func() {
-						projects, errL := client.AccountTeamProjects.List(account.Account.Id, team.Team.Id)
-						Expect(errL).NotTo(HaveOccurred())
-
-						if projects != nil {
-							var found bool
-							for _, p := range projects.Projects {
-								if p.ProjectName == projectName {
-									Expect(p.TeamType).To(Equal(projectType))
-									found = true
-								}
-							}
-
-							Expect(found).To(Equal(true), "cannot find project in the account team projects list")
-						}
-					})
-				})
-				AfterEach(func() {
-					if errD := client.AccountTeamProjects.Delete(account.Account.Id, team.Team.Id, projectName); errD != nil {
-						Fail("cannot delete account team project association :" + errD.Error())
-					}
-
-					if errD := client.Projects.Delete(projectName); errD != nil {
-						Fail("cannot delete project :" + errD.Error())
-					}
-				})
-
-			})
-		})
-
-		AfterEach(func() {
-			if errD := client.AccountTeams.Delete(account.Account.Id, team.Team.Id); errD != nil {
-				Fail("cannot delete account team :" + errD.Error())
+			if errD := client.AccountTeamInvites.Delete(account.Account.Id, team.Team.Id, memberEmail); errD != nil {
+				Fail("cannot delete an invitation :" + errD.Error())
 			}
 		})
 
-	})
+		// Account project
+		var (
+			projectName string
+			projectType = "admin"
+		)
 
-	Context("AccountAuthentications", func() {
-		It("check authentication methods", func() {
+		It("Account projects creation ", func() {
+			projectName = "test-acc-pr" + strconv.Itoa(rand.Int())
+
+			By("Create project")
+			if _, errP := client.Projects.Create(CreateProjectRequest{
+				Project:   projectName,
+				AccountId: account.Account.Id,
+			}); errP != nil {
+				Fail("cannot create project :" + errP.Error())
+			}
+
+			By("Create account team project association")
+			if errTP := client.AccountTeamProjects.Create(
+				account.Account.Id,
+				team.Team.Id,
+				AccountTeamProject{ProjectName: projectName, TeamType: projectType}); errTP != nil {
+				Fail("cannot create account team project association:" + errTP.Error())
+			}
+		})
+
+		It("Account team project association should be in the list", func() {
+			projects, errL := client.AccountTeamProjects.List(account.Account.Id, team.Team.Id)
+			Expect(errL).NotTo(HaveOccurred())
+
+			if projects != nil {
+				var found bool
+				for _, p := range projects.Projects {
+					if p.ProjectName == projectName {
+						Expect(p.TeamType).To(Equal(projectType))
+						found = true
+					}
+				}
+
+				Expect(found).To(Equal(true), "cannot find project in the account team projects list")
+			}
+		})
+
+		It("AccountAuthentications check authentication methods", func() {
 			list, errL := client.AccountAuthentications.List(account.Account.Id)
 			Expect(errL).NotTo(HaveOccurred())
 			Expect(list.AuthenticationMethods).NotTo(BeEmpty(), "default auth methods should be created automatically")
 		})
 
-		It("add new one", func() {
+		It("AccountAuthentications  add new one", func() {
 			resp, errL := client.AccountAuthentications.Create(account.Account.Id, AccountAuthenticationMethod{
 				Name: "test-auth",
 				Type: "saml",
@@ -204,11 +151,32 @@ var _ = Describe("Accounts", func() {
 			Expect(resp.APIResponse.Message).To(BeEmpty())
 			Expect(resp.APIResponse.Errors).To(BeEmpty())
 		})
-	})
 
-	AfterEach(func() {
-		if errD := client.Accounts.Delete(account.Account.Id); errD != nil {
-			Fail("cannot delete account :" + errD.Error())
-		}
+		It("remove all the stuff", func() {
+			if errD := client.AccountTeamProjects.Delete(account.Account.Id, team.Team.Id, projectName); errD != nil {
+				Fail("cannot delete account team project association :" + errD.Error())
+			}
+
+			if errD := client.Projects.Delete(projectName); errD != nil {
+				Fail("cannot delete project :" + errD.Error())
+			}
+
+			if list, errL := client.AccountTeamMembers.List(account.Account.Id, team.Team.Id); errL != nil {
+				Fail("cannot get a list of account team members:" + errL.Error())
+			} else {
+				for _, m := range list.Members {
+					if errD := client.AccountTeamMembers.Delete(account.Account.Id, team.Team.Id, m.UserEmail); errD != nil {
+						Fail("cannot delete account team member:" + errD.Error())
+					}
+				}
+			}
+			if errD := client.AccountTeams.Delete(account.Account.Id, team.Team.Id); errD != nil {
+				Fail("cannot delete account team :" + errD.Error())
+			}
+
+			if errD := client.Accounts.Delete(account.Account.Id); errD != nil {
+				Fail("cannot delete account :" + errD.Error())
+			}
+		})
 	})
 })

c53799f9bbbd8730ddd28036e5cbf2401f4e9496
Michael Hoffmann <michael.hoffmann@aiven.io>
2021-11-22 09:30:08 +0100

    add dynamic disk size support

--- service_types.go
+++ service_types.go
@@ -4,13 +4,22 @@
 package aiven
 
 type (
-	// GetServiceTypesResponse Aiven API request
+	// GetServicePlanResponse Aiven API request
 	// GET https://api.aiven.io/v1/project/<project>/service-types/<service_type>/plans/<service_plan>
-	GetServiceTypesResponse struct {
+	GetServicePlanResponse struct {
 		APIResponse
 		DiskSpaceCapMB  int `json:"disk_space_cap_mb"`
 		DiskSpaceMB     int `json:"disk_space_mb"`
 		DiskSpaceStepMB int `json:"disk_space_step_mb"`
+		//TODO: remaining fields
+	}
+
+	// GetServicePlanPricingResponse Aiven API request
+	// GET https://api.aiven.io/v1/project/<project>/pricing/service-types/<service_type>/plans/<service_plan>/cloud/<cloud>
+	GetServicePlanPricingResponse struct {
+		APIResponse
+		BasePriceUSD           string `json:"base_price_usd"`
+		ExtraDiskPricePerGBUSD string `json:"extra_disk_price_per_gb_usd"`
 		//TODO: remaining fields
 	}
 
@@ -21,14 +30,28 @@ type (
 )
 
 // Get fetches the service plan from Aiven
-func (h *ServiceTypesHandler) Get(project, serviceType, servicePlan string) (*GetServiceTypesResponse, error) {
+func (h *ServiceTypesHandler) GetPlan(project, serviceType, servicePlan string) (*GetServicePlanResponse, error) {
 	path := buildPath("project", project, "service-types", serviceType, "plans", servicePlan)
 	bts, err := h.client.doGetRequest(path, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	var r GetServiceTypesResponse
+	var r GetServicePlanResponse
+	errR := checkAPIResponse(bts, &r)
+
+	return &r, errR
+}
+
+// Get fetches the pricing for the service plan from Aiven
+func (h *ServiceTypesHandler) GetPlanPricing(project, serviceType, servicePlan, cloudName string) (*GetServicePlanPricingResponse, error) {
+	path := buildPath("project", project, "pricing", "service-types", serviceType, "plans", servicePlan, "clouds", cloudName)
+	bts, err := h.client.doGetRequest(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var r GetServicePlanPricingResponse
 	errR := checkAPIResponse(bts, &r)
 
 	return &r, errR

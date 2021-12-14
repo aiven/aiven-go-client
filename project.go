9b3a36e6abd2680ffdeeca8148c5bdeeb760415c
Ivan Savciuc <ivan.savciuc@aiven.io>
2021-12-14 09:15:25 +0200

    make account_id field as option for creation request

--- project.go
+++ project.go
@@ -42,7 +42,7 @@ type (
 		CopyFromProject              string           `json:"copy_from_project,omitempty"`
 		CountryCode                  *string          `json:"country_code,omitempty"`
 		Project                      string           `json:"project"`
-		AccountId                    string           `json:"account_id"`
+		AccountId                    *string          `json:"account_id,omitempty"`
 		TechnicalEmails              *[]*ContactEmail `json:"tech_emails,omitempty"`
 		BillingCurrency              string           `json:"billing_currency,omitempty"`
 		VatID                        *string          `json:"vat_id,omitempty"`

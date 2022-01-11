13cca468be76765c810da9786845fc7b7150b023
Adam Gandelman <adam.gandelman@auth0.com>
2022-01-11 15:44:35 -0800

    Add Associate/Dissociate/Get to static ips

--- static_ips.go
+++ static_ips.go
@@ -1,5 +1,7 @@
 package aiven
 
+import "fmt"
+
 type (
 	// StaticIPsHandler aiven go-client handler for static ips
 	StaticIPsHandler struct {
@@ -17,7 +19,7 @@ type (
 	CreateStaticIPResponse struct {
 		APIResponse
 
-		staticIP
+		StaticIP
 	}
 
 	// DeleteStaticIPRequest Aiven API request
@@ -31,11 +33,17 @@ type (
 	ListStaticIPResponse struct {
 		APIResponse
 
-		StaticIPs []staticIP `json:"static_ips"`
+		StaticIPs []StaticIP `json:"static_ips"`
+	}
+
+	// AssociateStaticIPRequest Aiven API request
+	// POST https://api.aiven.io/v1/project/<project>/static-ips/<static-ip>/association
+	AssociateStaticIPRequest struct {
+		ServiceName string `json:"service_name"`
 	}
 
 	// shared fields by some responses
-	staticIP struct {
+	StaticIP struct {
 		CloudName         string `json:"cloud_name"`
 		IPAddress         string `json:"ip_address"`
 		ServiceName       string `json:"service_name"`
@@ -69,6 +77,32 @@ func (h *StaticIPsHandler) Delete(project string, req DeleteStaticIPRequest) err
 	return checkAPIResponse(bts, nil)
 }
 
+// Get retrieves a Static IP
+// NOTE: API does not support GET /v1/project/{project}/static-ips/{static-ip-id},
+// need to fetch all, filter by ID and fake a 404 if nothing is found
+func (h *StaticIPsHandler) Get(project, staticIPID string) (*StaticIP, error) {
+	staticIPs, err := h.List(project)
+	if err != nil {
+		return nil, err
+	}
+
+	var targetStaticIP StaticIP
+
+	for _, staticIP := range staticIPs.StaticIPs {
+		if staticIP.StaticIPAddressID == staticIPID {
+			targetStaticIP = staticIP
+		}
+	}
+
+	if targetStaticIP.StaticIPAddressID == "" {
+		return nil, Error{
+			Message: fmt.Sprintf("static ip not found by id:%s", staticIPID),
+			Status:  404,
+		}
+	}
+	return &targetStaticIP, nil
+}
+
 // List lists all static ips
 func (h *StaticIPsHandler) List(project string) (*ListStaticIPResponse, error) {
 	path := buildPath("project", project, "static-ips")
@@ -81,4 +115,24 @@ func (h *StaticIPsHandler) List(project string) (*ListStaticIPResponse, error) {
 	errR := checkAPIResponse(bts, &r)
 
 	return &r, errR
+}
+
+func (h *StaticIPsHandler) Associate(project, staticIPID string, req AssociateStaticIPRequest) error {
+	path := buildPath("project", project, "static-ips", staticIPID, "association")
+	rsp, err := h.client.doPostRequest(path, req)
+	if err != nil {
+		return err
+	}
+
+	return checkAPIResponse(rsp, nil)
+}
+
+func (h *StaticIPsHandler) Dissociate(project, staticIPID string) error {
+	path := buildPath("project", project, "static-ips", staticIPID, "association")
+	rsp, err := h.client.doDeleteRequest(path, nil)
+	if err != nil {
+		return err
+	}
+
+	return checkAPIResponse(rsp, nil)
 }

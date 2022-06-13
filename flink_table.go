03d2190dd38e371d2acb15dfbadb4b36a093c4a8
Ivan Savciuc <ivan.savciuc@aiven.io>
2022-06-13 14:47:14 +0300

    update flink API request with the optional upsert kafka

--- flink_table.go
+++ flink_table.go
@@ -9,19 +9,19 @@ type (
 	// CreateFlinkTableRequest Aiven API request
 	// POST https://api.aiven.io/v1/project/<project>/service/<service_name>/flink/table
 	CreateFlinkTableRequest struct {
-		Name                    string                `json:"name"`
-		SchemaSQL               string                `json:"schema_sql"`
-		IntegrationId           string                `json:"integration_id"`
-		JDBCTable               string                `json:"jdbc_table,omitempty"`
-		KafkaConnectorType      string                `json:"kafka_connector_type,omitempty"`
-		KafkaTopic              string                `json:"kafka_topic,omitempty"`
-		KafkaKeyFields          []string              `json:"kafka_key_fields,omitempty"`
-		KafkaKeyFormat          string                `json:"kafka_key_format,omitempty"`
-		KafkaValueFormat        string                `json:"kafka_value_format,omitempty"`
-		KafkaStartupMode        string                `json:"kafka_startup_mode,omitempty"`
-		LikeOptions             string                `json:"like_options,omitempty"`
-		KafkaValueFieldsInclude string                `json:"kafka_value_fields_include,omitempty"`
-		UpsertKafka             FlinkTableUpsertKafka `json:"upsert_kafka,omitempty"`
+		Name                    string                 `json:"name"`
+		SchemaSQL               string                 `json:"schema_sql"`
+		IntegrationId           string                 `json:"integration_id"`
+		JDBCTable               string                 `json:"jdbc_table,omitempty"`
+		KafkaConnectorType      string                 `json:"kafka_connector_type,omitempty"`
+		KafkaTopic              string                 `json:"kafka_topic,omitempty"`
+		KafkaKeyFields          []string               `json:"kafka_key_fields,omitempty"`
+		KafkaKeyFormat          string                 `json:"kafka_key_format,omitempty"`
+		KafkaValueFormat        string                 `json:"kafka_value_format,omitempty"`
+		KafkaStartupMode        string                 `json:"kafka_startup_mode,omitempty"`
+		LikeOptions             string                 `json:"like_options,omitempty"`
+		KafkaValueFieldsInclude string                 `json:"kafka_value_fields_include,omitempty"`
+		UpsertKafka             *FlinkTableUpsertKafka `json:"upsert_kafka,omitempty"`
 	}
 
 	FlinkTableUpsertKafka struct {

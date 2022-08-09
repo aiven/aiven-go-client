4c4fe0df72ca3b2102d6b8ade67cdb12816742b0
Aleksander Zaruczewski <aleks@aiven.io>
2022-08-09 18:45:13 +0300

    add user configuration option endpoints support

--- project.go
+++ project.go
@@ -97,6 +97,72 @@ type (
 		ServiceName string `json:"service_name"`
 		Time        string `json:"time"`
 	}
+
+	// ProjectServiceTypesResponse is the response from Aiven for listing project service types
+	ProjectServiceTypesResponse struct {
+		APIResponse
+		ServiceTypes map[string]ServiceType `json:"service_types"`
+	}
+
+	// ServiceType represents a service type
+	ServiceType struct {
+		DefaultVersion         string        `json:"default_version"`
+		Description            string        `json:"description"`
+		LatestAvailableVersion string        `json:"latest_available_version"`
+		ServicePlans           []ServicePlan `json:"service_plans"`
+		UserConfigSchema       interface{}   `json:"user_config_schema"`
+	}
+
+	// ServicePlan represents a service plan
+	ServicePlan struct {
+		BackupConfig     *BackupConfig `json:"backup_config"`
+		MaxMemoryPercent int           `json:"max_memory_percent"`
+		NodeCount        int           `json:"node_count"`
+		Regions          interface{}   `json:"regions"`
+		ServicePlan      string        `json:"service_plan"`
+		ServiceType      string        `json:"service_type"`
+	}
+
+	// BackupConfig represents a backup config
+	BackupConfig struct {
+		FrequentIntervalMinutes    int    `json:"frequent_interval_minutes"`
+		FrequentOldestAgeMinutes   int    `json:"frequent_oldest_age_minutes"`
+		InfrequentIntervalMinutes  int    `json:"infrequent_interval_minutes"`
+		InfrequentOldestAgeMinutes int    `json:"infrequent_oldest_age_minutes"`
+		Interval                   int    `json:"interval"`
+		MaxCount                   int    `json:"max_count"`
+		RecoveryMode               string `json:"recovery_mode"`
+	}
+
+	// ProjectIntegrationTypesResponse is the response from Aiven for listing project integration types
+	ProjectIntegrationTypesResponse struct {
+		APIResponse
+		IntegrationTypes []IntegrationType `json:"integration_types"`
+	}
+
+	// IntegrationType represents an integration type
+	IntegrationType struct {
+		DestDescription    string      `json:"dest_description"`
+		DestServiceTypes   []string    `json:"dest_service_types"`
+		IntegrationType    string      `json:"integration_type"`
+		SourceDescription  string      `json:"source_description"`
+		SourceServiceTypes []string    `json:"source_service_types"`
+		UserConfigSchema   interface{} `json:"user_config_schema"`
+	}
+
+	// ProjectIntegrationEndpointTypesResponse is the response from Aiven for listing project integration endpoint types
+	ProjectIntegrationEndpointTypesResponse struct {
+		APIResponse
+		EndpointTypes []EndpointType `json:"endpoint_types"`
+	}
+
+	// EndpointType represents an endpoint type
+	EndpointType struct {
+		EndpointType     string      `json:"endpoint_type"`
+		ServiceTypes     []string    `json:"service_types"`
+		Title            string      `json:"title"`
+		UserConfigSchema interface{} `json:"user_config_schema"`
+	}
 )
 
 // ContactEmailFromStringSlice creates []*ContactEmail from string slice
@@ -193,7 +259,7 @@ func (h *ProjectsHandler) List() ([]*Project, error) {
 	return r.Projects, errR
 }
 
-// EventLog Get project event log entries
+// GetEventLog returns project event log entries
 func (h *ProjectsHandler) GetEventLog(project string) ([]*ProjectEvent, error) {
 	bts, err := h.client.doGetRequest(buildPath("project", project, "events"), nil)
 	if err != nil {
@@ -204,4 +270,43 @@ func (h *ProjectsHandler) GetEventLog(project string) ([]*ProjectEvent, error) {
 	errR := checkAPIResponse(bts, &r)
 
 	return r.Events, errR
+}
+
+// ServiceTypes returns all the available service types
+func (h *ProjectsHandler) ServiceTypes(project string) (map[string]ServiceType, error) {
+	bts, err := h.client.doGetRequest(buildPath("project", project, "service_types"), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var r ProjectServiceTypesResponse
+	err = checkAPIResponse(bts, &r)
+
+	return r.ServiceTypes, err
+}
+
+// IntegrationTypes returns all the available integration types
+func (h *ProjectsHandler) IntegrationTypes(project string) ([]IntegrationType, error) {
+	bts, err := h.client.doGetRequest(buildPath("project", project, "integration_types"), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var r ProjectIntegrationTypesResponse
+	err = checkAPIResponse(bts, &r)
+
+	return r.IntegrationTypes, err
+}
+
+// IntegrationEndpointTypes returns all the available integration endpoint types
+func (h *ProjectsHandler) IntegrationEndpointTypes(project string) ([]EndpointType, error) {
+	bts, err := h.client.doGetRequest(buildPath("project", project, "integration_endpoint_types"), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var r ProjectIntegrationEndpointTypesResponse
+	err = checkAPIResponse(bts, &r)
+
+	return r.EndpointTypes, err
 }
